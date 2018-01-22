9688acc6221528355e9eda6a5e43df173dec5269
testa <testa@intre.it>
2018-01-22 16:40:26 +0100

    package refactoring

--- src/classical/shift/shift_test.go
+++ src/classical/shift/shift_test.go
@@ -1,8 +1,8 @@
-package classical_test
+package shift_test
 
 import (
 	"testing"
-	"../classical"
+	"../../classical/shift"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -11,85 +11,85 @@ import (
 func TestEncode_1a(t *testing.T) {
 	p := "aaaaa"
 	k := 1
-	assert.Equal(t, "bbbbb", classical.Encode (p, k))
+	assert.Equal(t, "bbbbb", shift.Encode (p, k))
 }
 
 func TestEncode_1abc(t *testing.T) {
 	p := "abc"
 	k := 1
-	assert.Equal(t, "bcd", classical.Encode (p, k))
+	assert.Equal(t, "bcd", shift.Encode (p, k))
 }
 
 func TestEncode_1az(t *testing.T) {
 	p := "az"
 	k := 1
-	assert.Equal(t, "ba", classical.Encode (p, k))
+	assert.Equal(t, "ba", shift.Encode (p, k))
 }
 
 func TestEncode_2a(t *testing.T) {
 	p := "aaaaa"
 	k := 2
-	assert.Equal(t, "ccccc", classical.Encode (p, k))
+	assert.Equal(t, "ccccc", shift.Encode (p, k))
 }
 
 func TestEncode_2abc(t *testing.T) {
 	p := "abc"
 	k := 2
-	assert.Equal(t, "cde", classical.Encode (p, k))
+	assert.Equal(t, "cde", shift.Encode (p, k))
 }
 
 func TestEncode_2az(t *testing.T) {
 	p := "az"
 	k := 2
-	assert.Equal(t, "cb", classical.Encode (p, k))
+	assert.Equal(t, "cb", shift.Encode (p, k))
 }
 
 func TestEncode_26az(t *testing.T) {
 	p := "az"
 	k := 26
-	assert.Equal(t, "az", classical.Encode (p, k))
+	assert.Equal(t, "az", shift.Encode (p, k))
 }
 
 func TestDecode_1a(t *testing.T) {
 	c := "aaaaa"
 	k := 1
-	assert.Equal(t, "zzzzz", classical.Decode (c, k))
+	assert.Equal(t, "zzzzz", shift.Decode (c, k))
 }
 
 func TestDecode_1abc(t *testing.T) {
 	c := "abc"
 	k := 1
-	assert.Equal(t, "zab", classical.Decode (c, k))
+	assert.Equal(t, "zab", shift.Decode (c, k))
 }
 
 func TestDecode_1az(t *testing.T) {
 	c := "az"
 	k := 1
-	assert.Equal(t, "zy", classical.Decode (c, k))
+	assert.Equal(t, "zy", shift.Decode (c, k))
 }
 
 func TestDecode_2a(t *testing.T) {
 	c := "aaaaa"
 	k := 2
-	assert.Equal(t, "yyyyy", classical.Decode (c, k))
+	assert.Equal(t, "yyyyy", shift.Decode (c, k))
 }
 
 func TestDecode_2abc(t *testing.T) {
 	c := "abc"
 	k := 2
-	assert.Equal(t, "yza", classical.Decode (c, k))
+	assert.Equal(t, "yza", shift.Decode (c, k))
 }
 
 func TestDecode_2az(t *testing.T) {
 	c := "az"
 	k := 2
-	assert.Equal(t, "yx", classical.Decode (c, k))
+	assert.Equal(t, "yx", shift.Decode (c, k))
 }
 
 func TestDecode_26az(t *testing.T) {
 	c := "az"
 	k := 26
-	assert.Equal(t, "az", classical.Decode (c, k))
+	assert.Equal(t, "az", shift.Decode (c, k))
 }
 
 
@@ -97,6 +97,6 @@ func TestDecode_26az(t *testing.T) {
 func TestComplete(t *testing.T) {
 	p := "abcdefghijklmnopqrstuvwxyz"
 	k := 21
-	c := classical.Encode (p, k)
-	assert.Equal(t, p, classical.Decode (c, k))
+	c := shift.Encode (p, k)
+	assert.Equal(t, p, shift.Decode (c, k))
 }
