88b3cd8e07cafbea9cd1a96ffa2d39821986ddbc
Lorenzo Testa <testa.lorenzo@hotmail.com>
2018-02-27 22:22:40 +0100

    frequencytable updated and covered by tests

--- src/utilities/frequencyTable.go
+++ src/utilities/frequencyTable.go
@@ -2,19 +2,20 @@ package utilities
 
 import (
 	"math"
+	"sort"
 )
 
 type Value struct {
-	Key rune
-	Value float64
+	Key  rune
+	Rate float64
 }
 
 
-type FrequencyTable []Value
+type FrequencyTable []*Value
 
 
 func (p FrequencyTable) Len() int           { return len(p) }
-func (p FrequencyTable) Less(i, j int) bool { return p[i].Value < p[j].Value }
+func (p FrequencyTable) Less(i, j int) bool { return p[i].Key < p[j].Key || p[i].Rate < p[j].Rate}
 func (p FrequencyTable) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
 
@@ -27,23 +28,23 @@ func NewFrequencyTable(s string) FrequencyTable {
 	}
 
 	for i, v := range count {
-		result = append(result, Value{i, v})
+		result = append(result, &Value{i, v})
 	}
 
 	return result
 }
 
 func (ft *FrequencyTable) CalcPerc(l int) {
-	k := float64(l / 100)
+	k := 1 / float64(l)
 	for _, v := range *ft {
-		v.Value = v.Value * k
+		v.Rate = v.Rate * k
 	}
 }
 
 func (ft *FrequencyTable) GetSmallerThan(x float64) []rune {
 	result := make([]rune, 0, len(*ft))
 	for _, v := range *ft {
-		if v.Value <= x {
+		if v.Rate <= x {
 			result = append(result, v.Key)
 		} else {
 			break
@@ -56,10 +57,59 @@ func (ft *FrequencyTable) GetSmallerThan(x float64) []rune {
 func (ft *FrequencyTable) CalcDeltas(count float64) FrequencyTable {
 	deltas := make(FrequencyTable, len(*ft))
 	for i, v := range *ft {
-		deltas[i] = Value{ v.Key, math.Abs(v.Value - count)}
+		deltas[i] = &Value{ v.Key, math.Abs(v.Rate - count)}
 	}
 	return deltas
 }
+
+
+func (a FrequencyTable) Equal(b FrequencyTable) bool {
+	if len(a) != len(b) { return false }
+	sort.Sort(a)
+	sort.Sort(b)
+
+	for i := 0; i < len(a); i++ {
+
+		if !a[i].Equal(b[i]) { return false }
+	}
+	return true
+}
+
+
+func (a *Value) Equal(b *Value) bool {
+	return a.Key == b.Key && a.Rate == b.Rate
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/utilities/frequencyTable_test.go
+++ src/utilities/frequencyTable_test.go
@@ -7,15 +7,57 @@ import (
 )
 
 
-func TestGetSmallerThan_0(t *testing.T) {
-	ol := FrequencyTable{{'b', 0.015}, {'c', 0.028},
-						{'d', 0.043}, {'a', 0.082}}
-	expected := []rune{'b','c','d'}
-	assert.Equal(t, expected, ol.GetSmallerThan(0.050))
+func TestFrequencyTable_NewFrequencyTable_0(t *testing.T) {
+	ft := NewFrequencyTable("abbccddd")
+	expected := FrequencyTable{{'a', 1}, {'b', 2}, {'c', 2}, {'d', 3}}
+	assert.Equal(t, true, ft.Equal(expected))
 }
 
-func TestCalcDeltas_0(t *testing.T) {
+
+func TestFrequencyTable_CalcPerc_0(t *testing.T) {
+	s := "abcd"
+	ft := NewFrequencyTable(s)
+	expected := FrequencyTable{{'a', 0.25}, {'b', 0.25}, {'c', 0.25}, {'d', 0.25}}
+	ft.CalcPerc(len(s))
+	assert.Equal(t, true, ft.Equal(expected))
+}
+
+
+func TestFrequencyTable_GetSmallerThan_0(t *testing.T) {
+	ft := FrequencyTable{{'b', 0.015}, {'c', 0.028},
+						{'d', 0.043}, {'a', 0.082}}
+	expected := []rune{'b','c','d'}
+	assert.Equal(t, expected, ft.GetSmallerThan(0.050))
+}
+
+
+func TestFrequencyTable_CalcDeltas_0(t *testing.T) {
 	ft := FrequencyTable{{'a', 0.082}, {'b', 0.015}, {'c', 0.028}, {'d', 0.043}}
 	expected :=  FrequencyTable{{'a', 0.918}, {'b', 0.985}, {'c', 0.972}, {'d', 0.957}}
 	assert.Equal(t, expected, ft.CalcDeltas(1))
+}
+
+func TestFrequencyTable_Equal(t *testing.T) {
+	ft := FrequencyTable{{'a', 1}, {'b', 2}, {'c', 2}, {'d', 3}}
+	ft2 := FrequencyTable{{'a', 1}, {'b', 2}, {'c', 2}, {'d', 3}}
+	ft3 := FrequencyTable{{'a', 1}, {'b', 1}, {'c', 2}, {'d', 3}}
+	ft4 := FrequencyTable{{'a', 1}, {'a', 2}, {'c', 2}, {'d', 3}}
+
+	assert.Equal(t, true, ft.Equal(ft))
+	assert.Equal(t, true, ft.Equal(ft2))
+	assert.Equal(t, false, ft.Equal(ft3))
+	assert.Equal(t, false, ft.Equal(ft4))
+}
+
+
+func TestValue_Equal(t *testing.T) {
+	v := Value{'a', 1}
+	v2 := Value{'a', 1}
+	v3 := Value{'b', 1}
+	v4 := Value{'a', 2}
+
+	assert.Equal(t, true, v.Equal(&v))
+	assert.Equal(t, true, v.Equal(&v2))
+	assert.Equal(t,false, v.Equal(&v3))
+	assert.Equal(t,false, v.Equal(&v4))
 }
